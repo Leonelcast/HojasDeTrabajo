86e0de2bb645de576b8766c139c4f1f1c0ab2614
Leonelcast <Leonelcas.izq@gmail.com>
2018-02-01 14:33:27 -0600

    tareas

---
+++ HojaDeTrabajo2/Tarea/Tarea/Persona.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea
+{
+    public class Persona
+    {
+
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public QueHaceres Tareas { get; set; }
+
+        public Persona()
+        {
+            Tareas = new QueHaceres();
+        }
+
+        public Persona(string nombre, string apellido)
+        {
+            Nombre = nombre;
+            Apellido = apellido;
+            Tareas = new QueHaceres();
+        }
+        public void AgregarQueHacer(QueHacer queHacer)
+        {
+            Tareas.AgregarTarea(queHacer);
+        }
+
+        public void CompletarQueHacer()
+        {
+            Tareas.CompletarQueHacer();
+        }
+
+        public bool EstaDisponible()
+        {
+            if (Tareas.Disponible())
+            {
+                return true;
+            }
+            return false;
+        }
+
+   
+    }
+}
---
+++ HojaDeTrabajo2/Tarea/Tarea/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+                Persona P1 = new Persona();
+                Persona P2 = new Persona();
+
+                QueHacer queHacer1 = new QueHacer("Casa", "Almuerzo",  "Hacer el almuerzo en la casa");
+                QueHacer queHacer2 = new QueHacer("Colonia","Pasear perro", "Pasear a mi perro en la colonia");
+                QueHacer queHacer3 = new QueHacer("Gimnasio","Ejercicio", "Hacer ejercicio en el gimansio");
+                QueHacer queHacer4 = new QueHacer("Condominio","Lavar carro", "Lavar el carro en el condominio");
+                QueHacer queHacer5 = new QueHacer("Casa","Platos", "Lavar los platos de la casa");
+                QueHacer queHacer6 = new QueHacer("Casa","Tarea", "Hacer la tarea de calculo en la casa");
+
+                P1.AgregarQueHacer(queHacer1);
+                P1.AgregarQueHacer(queHacer2);
+                P1.AgregarQueHacer(queHacer3);
+
+                P2.AgregarQueHacer(queHacer4);
+                P2.AgregarQueHacer(queHacer5);
+                P2.AgregarQueHacer(queHacer6);
+
+                P1.CompletarQueHacer();
+
+                P2.CompletarQueHacer();
+                P2.CompletarQueHacer();
+                P2.CompletarQueHacer();
+
+                Console.WriteLine("Persona 1 esta disponible? " + P1.EstaDisponible());
+                Console.WriteLine("Persona 2 esta disponible?" + P2.EstaDisponible());
+                Console.ReadLine();
+            }
+        }
+    }
+
+
