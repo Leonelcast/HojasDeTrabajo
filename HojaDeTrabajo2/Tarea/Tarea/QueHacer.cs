a96381ff8f8575293aee66cda1bbe389787b25e2
Leonelcast <Leonelcas.izq@gmail.com>
2018-02-01 12:18:45 -0600

    tarea de informatica

---
+++ HojaDeTrabajo2/Tarea/Tarea/QueHacer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea
+{
+    public enum Estado
+    {
+        EnProgreso,
+        Terminado
+    }
+
+    public class QueHacer
+    {
+        public string Lugar { get; set; }
+        public string Titulo { get; set; }
+        public string Descripcion { get; set; }
+        public Estado estado { get; private set; }
+
+        public void Completar()
+        {
+            estado = Estado.Terminado;
+        }
+
+        public QueHacer(string lugar, string titulo, string descripcion)
+        {
+            Lugar = lugar;
+            Titulo = titulo;
+            Descripcion = descripcion;
+            estado = Estado.EnProgreso;
+        }
+    }
+}
