06f8d0ccb2eed7c253f6cda73a6fd52011ee9981
Leonelcast <Leonelcas.izq@gmail.com>
2018-03-01 18:43:51 -0600

    tarea

---
+++ HojaDeTrabajo5/Reduce/ReduceTest/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Reduce;
+
+namespace ReduceTest
+{
+        [TestClass]
+        public class UnitTest
+        {
+            [TestMethod]
+            public void TestReduce()
+            {
+                int[] valores = new int[] { 1, 3, 5, 7 };
+                Assert.AreEqual(Program.ReduceInt(valores, 0, (acumulador, valor) => acumulador + valor), 16);
+            }
+
+            [TestMethod]
+            public void TestGenerico()
+            {
+                string[] valores = new string[] { "paseando", "a", "mi", "perro" };
+                Assert.AreEqual(Program.Reduce<string, int>(valores, 0, (acumulador, valor) => acumulador + valor.Length), 16);
+            }
+        }
+    }
