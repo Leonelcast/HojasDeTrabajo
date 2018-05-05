1c963c14a56d150b176a710cf6ab05a56723127e
Leonelcast <Leonelcas.izq@gmail.com>
2018-05-04 21:36:09 -0600

    tarea

---
+++ HojaDeTrabajo8/ParseTree/ParseTree.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ParseTree
+{
+    public class ParseTree : BinaryTree<string>
+    {
+        public ParseTree(string valor, IBinTree<string> derecho, IBinTree<string> izquierdo) : base(valor, derecho, izquierdo)
+        {
+
+        }
+
+        public ParseTree(string valor) : base(valor)
+        {
+            this.Valor = valor;
+        }
+
+        public override void ExpandirSuma()
+        {
+
+            int lastSuma = Valor.LastIndexOf("+");
+
+            if (lastSuma == -1)
+                return;
+
+            this.Izquierdo = new ParseTree(Valor.Substring(0, lastSuma));
+            this.Derecho = new ParseTree(Valor.Substring(lastSuma + 1, Valor.Length - lastSuma - 1));
+
+            this.Valor = "+";
+
+            this.Izquierdo.Expandir();
+            this.Derecho.Expandir();
+        }
+
+        public override void ExpandirMultiplicacion()
+        {
+
+            int lastSuma = Valor.LastIndexOf("*");
+
+            if (lastSuma == -1)
+                return;
+
+            this.Izquierdo = new ParseTree(Valor.Substring(0, lastSuma));
+            this.Derecho = new ParseTree(Valor.Substring(lastSuma + 1, Valor.Length - lastSuma - 1));
+
+            this.Valor = "*";
+
+            this.Izquierdo.Expandir();
+            this.Derecho.Expandir();
+        }
+
+        public override void Expandir()
+        {
+            ExpandirSuma();
+            ExpandirMultiplicacion();
+        }
+
+        public override int Resultado()
+        {
+            switch (Valor)
+            {
+                case "+":
+                    return Izquierdo.Resultado() + Derecho.Resultado();
+                case "*":
+                    return Izquierdo.Resultado() * Derecho.Resultado();
+            }
+
+            return Int32.Parse(Valor);
+        }
+
+    }
+}
---
+++ HojaDeTrabajo9/Heap/HeapTest/HeapTest.cs
@@ -0,0 +1,17 @@
+﻿using Heap;
+using System;
+using Xunit;
+
+namespace HeapTest
+{
+	public class HeapTest
+	{
+		[Fact]
+		public void HeapfyTest()
+		{
+			int[] arreglo = { 2, 4, 9, 5, 19, 12 };
+			Program.Heapify(arreglo);
+			Assert.Equal(new int[] { 19, 12, 9, 5, 4, 2 }, arreglo);			
+		}
+	}
+}
